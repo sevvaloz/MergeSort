961a78027854f76354bbd40ff72042d87a9eaecf
Şevval Özdamar <ozdamarsevval.01@gmail.com>
2023-01-29 02:52:39 +0300

    duzenleme yapildi.

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -5,17 +5,17 @@
         public static void Main(string[] args)
         {
             //array[basIndex...sonIndex]
-            int[] array = { 6, 5, 12, 10, 9, 1 };
+            int[] array = { 4, 1, 0, 7, 7, 2, 9 };
 
             //fonksiyonları çağırma
             Merge merge = new Merge();
             merge.divide(array, 0, array.Length - 1);
 
             //diziyi sıralayıp ekrana yazdırma
-            Console.WriteLine("sıralı dizi:");
+            Console.Write("sıralı dizi: ");
             for (int i = 0; i < array.Length; i++)
             {
-                Console.WriteLine(array[i] + " ");
+                Console.Write(array[i] + " ");
             }
         }
     }
